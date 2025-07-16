f47e240e21e2752bf5739a926e8f41f8caac296e
SDeGiuda <santiagodegiuda@gmail.com>
2025-07-16 13:02:45 -0300

    refactor: phpstan corrections

--- src/Backoffice/Cities/Domain/Actions/ListCitiesAction.php
+++ src/Backoffice/Cities/Domain/Actions/ListCitiesAction.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace Lightit\Backoffice\Cities\Domain\Actions;
 
-use Illuminate\Console\View\Components\Task;
+
 use Illuminate\Database\Eloquent\Collection;
 use Lightit\Backoffice\Cities\Domain\Models\City;
 
 class ListCitiesAction
 {
     /**
-     * @return Collection<int,Task>
+     * @return Collection<int, City>
      */
     public function execute(): Collection
     {
