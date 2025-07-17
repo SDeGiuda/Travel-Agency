06418f6a398429887ceb1f3c07309102348b384d
SDeGiuda <santiagodegiuda@gmail.com>
2025-07-17 15:09:51 -0300

    fix: fix FiltersDTO handling in List Airline Action, for it was outdated and expected array

--- src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
+++ src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
@@ -6,46 +6,48 @@ namespace Lightit\Backoffice\Airlines\Domain\Actions;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Lightit\Backoffice\Airlines\Domain\DataTransferObjects\FiltersDTO;
 use Lightit\Backoffice\Airlines\Domain\Models\Airline;
+
+use function PHPUnit\Framework\isNull;
 
 class ListAirlineAction
 {
     /**
      * @return Collection<int, Airline>
      */
-    public function execute(array $filters): Collection
+    public function execute(FiltersDTO $filters): Collection
     {
         /** @var Builder<Airline> $query */
         $query = Airline::query()->withCount('flights');
 
-        if (isset($filters['min_flight_count'])) {
+        if (!is_null($filters->minFlightCount)) {
             $query->where('flights_count', '>=', $filters['min_flight_count']);
         }
-        if (isset($filters['max_flight_count'])) {
+        if (!is_null($filters->maxFlightCount)) {
             $query->where('flights_count', '<=', $filters['max_flight_count']);
         }
-        if (isset($filters['destination_city'])) {
+        if (!is_null($filters->destinationId)) {
             $query->whereExists(function ($subQuery) use ($filters): void {
                 /** @var Builder<Airline> $subQuery */
                 $subQuery->select('flight_id')
                     ->from('flights')
                     ->whereColumn('airline_id', 'flights.airline_id')
-                    ->whereIn('destination_id', $filters['destination_city']);
+                    ->whereIn('destination_id', $filters->destinationId);
             });
         }
-        if (isset($filters['origin_city'])) {
+        if (!is_null($filters->originId)) {
             $query->whereExists(function ($subQuery) use ($filters): void {
                 /** @var Builder<Airline> $subQuery */
                 $subQuery->select('flight_id')
                     ->from('flights')
                     ->whereColumn('airline_id', 'flights.airline_id')
-                    ->whereIn('origin_id', $filters['origin_city']);
+                    ->whereIn('origin_id', $filters->originId);
             });
         }
-        if (isset($filters['order_by_name'])) {
-            /** @var string $order */
-            $order = $filters['order_by_name'];
-            $query->orderBy('name', $order);
+        if (!is_null($filters->orderByName)) {
+
+            $query->orderBy('name', $filters->orderByName);
         } else {
             $query->orderBy('id');
         }
